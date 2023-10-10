db895e1186b9ccd36b94222add51c4bb7f1e88f9
Victor Kruze Fiori <kruzefiori@hotmail.com>
2023-10-09 22:33:58 -0300

    API de instituto criada com os CRUDS

---
+++ backend/src/functions/apis/InstituteDelete.ts
@@ -0,0 +1,38 @@
+import ApiWrapper, {
+    ApiWrapperHandler,
+} from "../../utils/wrappers/apiWrapper";
+import res from "../../utils/wrappers/apiResponse";
+
+const handler: ApiWrapperHandler = async (conn, req) => {
+    const { institute_id } = req.params;
+
+    const institute = await conn.institutes.findUnique({
+        where: {
+            id: institute_id,
+        }
+    });
+
+    if (!institute) {
+        return res.error(404, null, "Institute not found");
+    }
+
+    await conn.institutes.delete({
+        where: {
+            id: institute_id,
+        }
+    });
+    return res.success(institute);
+}
+
+export default new ApiWrapper(handler)
+    .setSchemaValidator((schema) => ({
+        params: schema.object().shape({
+            institute_id: schema.string().required(),
+        }),
+    })).configure({
+        name: "Institute-Delete",
+        options: {
+            methods: ["DELETE"],
+            route: "/institute/{institute_id}",
+        },
+    });
