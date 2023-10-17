09d317456fac30e420f6916683934e0d3e7db115
Vitória Thaís <vitoriathdasilva@gmail.com>
2023-10-17 20:03:44 -0300

    add listUser route

--- frontend/src/Routes.tsx
+++ frontend/src/Routes.tsx
@@ -4,14 +4,17 @@ import {
     Route
 } from 'react-router-dom';
 import Login from './pages/Login';
-
+import Dashboard from './pages/Dashboard';
+import ListUsers from './components/ListUsers';
 
 export function AppRoutes() {
     return (
         <Router>
             <Routes>
-                <Route path='/login' element= {<Login/>}/>
-                <Route path='/' element= {<Login/>}/>
+                <Route path='/login' element= {<Login/>}/> 
+                <Route path='/' element= {<Dashboard/>}>
+                    <Route path='listusers' element= {<ListUsers/>}/>
+                </Route>
             </Routes>
         </Router>)
 }   
