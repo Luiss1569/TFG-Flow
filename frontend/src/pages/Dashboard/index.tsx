264f248405fda7f12abbf2f6d3793a1d11f50a91
Vitória Thaís <vitoriathdasilva@gmail.com>
2023-10-18 18:33:14 -0300

    restructure dashboard

--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -1,36 +1,47 @@
-import { Grid, GridItem, List, ListIcon, ListItem } from "@chakra-ui/react";
+import { Grid, GridItem, IconButton, useColorMode } from "@chakra-ui/react";
 import Sidebar from "../../components/Sidebar";
 import { Outlet } from "react-router-dom";
+import { SunIcon, MoonIcon } from '@chakra-ui/icons';
 
+function Dashboard() {
+  const { colorMode, toggleColorMode } = useColorMode();
 
-function Dashboard(){
-    return(
-      <Grid
-      templateAreas={`"header header"
-                      "nav main"
-                      "nav footer"`}
-      gridTemplateRows={'50px 1fr 30px'}
-      gridTemplateColumns={'200px 1fr'}
-      // h='200px'
-      gap='1'
-      // color='blackAlpha.700'
-      fontWeight='bold'
+  return (
+
+    <Grid
+      templateColumns='repeat(6,1fr)'
+      bg='primary'
+      
     >
-      <GridItem pl='2'area={'header'}>
-        Header
+
+      <GridItem
+        borderRight='1px' 
+        borderColor='secondary'
+        as='aside'
+        colSpan={{ base: 6, lg: 2, xl: 1 }}
+        p={{ base: '20px', lg: '30px' }}
+        minHeight={{ lg: '100vh' }}
+      >
+        <Sidebar />
       </GridItem>
-      <GridItem pl='2' area={'nav'}>
-        <Sidebar/>
+      <GridItem
+        as='main'
+        colSpan={{ base: 6, lg: 4, xl: 5 }}
+      >
+        <Outlet />
       </GridItem>
-      <GridItem pl='2' area={'main'}>
-        <Outlet/>
-      </GridItem>
-      <GridItem pl='2' area={'footer'}>
-        Footer
-      </GridItem>
+      <IconButton
+        aria-label="toggle theme"
+        rounded="full"
+        size="xs"
+        position="absolute"
+        bottom={4}
+        left={4}
+        onClick={toggleColorMode} icon={colorMode === "dark" ? <SunIcon /> : <MoonIcon />}
+      />
     </Grid>
-    
-    )
+
+  )
 }
 
 export default Dashboard;
