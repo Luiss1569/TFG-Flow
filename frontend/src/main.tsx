12cc9e7c1767b2f4cceab11796409343ddfff53d
Vitória Thaís <vitoriathdasilva@gmail.com>
2023-10-18 18:31:47 -0300

    add extendTheme

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import { ChakraProvider} from "@chakra-ui/react"
+import { ChakraProvider} from "@chakra-ui/react";
+import Theme from './styles/theme.ts';
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ChakraProvider>
-      <App /> 
+    <ChakraProvider theme={Theme}>
+      <App/> 
     </ChakraProvider>
   </React.StrictMode>,
 )
