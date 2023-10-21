ff1f2e8eb4fe9cd42db1de2ba17696d57fb05e8a
Victor Kruze Fiori <kruzefiori@hotmail.com>
2023-10-21 20:10:20 -0300

    testes com karate

---
+++ backend/tfgflowTests/src/test/java/tfgflow/tfgFlowTests.java
@@ -0,0 +1,18 @@
+package tfgflow;
+
+import com.intuit.karate.Results;
+import com.intuit.karate.Runner;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class tfgFlowTests {
+
+    @Test
+    void testParallel() {
+        Results results = Runner.path("classpath:tfgflow")
+                //.outputCucumberJson(true)
+                .parallel(5);
+        assertEquals(0, results.getFailCount(), results.getErrorMessages());
+    }
+
+}
