7b3d0e659164e5edf68dec70e456adad10199518
Vitória Thaís <vitoriathdasilva@gmail.com>
2023-10-24 20:30:49 -0300

    create private routes

---
+++ frontend/src/routes/private.routes.tsx
@@ -0,0 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Response from "../pages/Response";
+import Dashboard from "../pages/Dashboard";
+import ListUsers from "../components/ListUsers";
+
+export function PrivateRoutes() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Dashboard />} >
+          <Route path="listusers" element={<ListUsers />} />
+        </Route>
+        <Route path="/response/:slug" element={<Response />} />
+      </Routes>
+    </Router>
+  );
+}
