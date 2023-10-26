45fccb730d2d0898a0afeb09247066439673d8e9
Luiss1569 <rcsrcs1569@gmail.com>
2023-10-26 11:33:24 -0300

    refactor: sidebar

--- frontend/src/components/Sidebar/index.tsx
+++ frontend/src/components/Sidebar/index.tsx
@@ -6,8 +6,9 @@ import {
   Tooltip,
   Link as ChakraLink,
   TagLabel,
+  TagProps,
 } from "@chakra-ui/react";
-import { Link as ReactRouterLink } from "react-router-dom";
+import { Link as ReactRouterLink, useLocation } from "react-router-dom";
 
 import {
   BsHouse,
@@ -20,11 +21,7 @@ import {
 } from "react-icons/bs";
 import React from "react";
 
-type CustomCardProps = {
-  children: React.ReactNode;
-};
-
-const CustomCard = React.forwardRef<HTMLSpanElement, CustomCardProps>(
+const CustomCard = React.forwardRef<HTMLSpanElement, TagProps>(
   ({ children, ...rest }, ref) => (
     <Box p="0">
       <Tag
@@ -42,145 +39,65 @@ const CustomCard = React.forwardRef<HTMLSpanElement, CustomCardProps>(
   )
 );
 
+const items = [
+  { to: "/portal", label: "Dashboard", icon: BsHouse },
+  { to: "/portal/activities", label: "Atividades", icon: BsCardText },
+  { to: "/portal/users", label: "Usuários", icon: BsPerson },
+  { to: "/portal/institutes", label: "Instituições", icon: BsBuilding },
+  { to: "/portal/status", label: "Status", icon: BsTag },
+  { to: "/portal/forms", label: "Formulários", icon: BsFileEarmarkText },
+  { to: "/portal/reportings", label: "Relatórios", icon: BsPostcardFill },
+];
+
 function Sidebar() {
+  const location = useLocation();
+
   return (
     <List fontSize="xl" spacing={4}>
       <ListItem py={2}></ListItem>
-      <ListItem>
-        <ChakraLink
-          as={ReactRouterLink}
-          to="/portal"
-          _hover={{ textDecor: "none", color: "green_light" }}
-        >
-          <Tooltip
-            label="Dashboard"
-            aria-label="A tooltip"
-            hasArrow
-            size="md"
-            placement="right-end"
-          >
-            <CustomCard>
-              <BsHouse size={24} />
-            </CustomCard>
-          </Tooltip>
-        </ChakraLink>
-      </ListItem>
-      <ListItem>
-        <ChakraLink
-          as={ReactRouterLink}
-          to="/portal/activities"
-          _hover={{ textDecor: "none", color: "green_light" }}
-        >
-          <Tooltip
-            label="Atividades"
-            aria-label="A tooltip"
-            hasArrow
-            size="md"
-            placement="right-end"
-          >
-            <CustomCard>
-              <BsCardText size={24} />
-            </CustomCard>
-          </Tooltip>
-        </ChakraLink>
-      </ListItem>
-      <ListItem>
-        <ChakraLink
-          as={ReactRouterLink}
-          to="/portal/users"
-          _hover={{ textDecor: "none", color: "green_light" }}
-        >
-          <Tooltip
-            label="Usuários"
-            aria-label="A tooltip"
-            hasArrow
-            size="md"
-            placement="right-end"
-          >
-            <CustomCard>
-              <BsPerson size={24} />
-            </CustomCard>
-          </Tooltip>
-        </ChakraLink>
-      </ListItem>
-      <ListItem>
-        <ChakraLink
-          as={ReactRouterLink}
-          to="/portal/institutes"
-          _hover={{ textDecor: "none", color: "green_light" }}
-        >
-          <Tooltip
-            label="Instituições"
-            aria-label="A tooltip"
-            hasArrow
-            size="md"
-            placement="right-end"
-          >
-            <CustomCard>
-              <BsBuilding size={24} />
-            </CustomCard>
-          </Tooltip>
-        </ChakraLink>
-      </ListItem>
-      <ListItem>
-        <ChakraLink
-          as={ReactRouterLink}
-          to="/portal/status"
-          _hover={{ textDecor: "none", color: "green_light" }}
-        >
-          <Tooltip
-            label="Status"
-            aria-label="A tooltip"
-            hasArrow
-            size="md"
-            placement="right-end"
-          >
-            <CustomCard>
-              <BsTag size={24} />
-            </CustomCard>
-          </Tooltip>
-        </ChakraLink>
-      </ListItem>
-      <ListItem>
-        <ChakraLink
-          as={ReactRouterLink}
-          to="/portal/forms"
-          _hover={{ textDecor: "none", color: "green_light" }}
-        >
-          <Tooltip
-            label="Formulários"
-            aria-label="A tooltip"
-            hasArrow
-            size="md"
-            placement="right-end"
-          >
-            <CustomCard>
-              <BsFileEarmarkText size={24} />
-            </CustomCard>
-          </Tooltip>
-        </ChakraLink>
-      </ListItem>
-      <ListItem>
-        <ChakraLink
-          as={ReactRouterLink}
-          to="/portal//reportings"
-          _hover={{ textDecor: "none", color: "green_light" }}
-        >
-          <Tooltip
-            label="Relatórios"
-            aria-label="A tooltip"
-            hasArrow
-            size="md"
-            placement="right-end"
-          >
-            <CustomCard>
-              <BsPostcardFill size={24} />
-            </CustomCard>
-          </Tooltip>
-        </ChakraLink>
-      </ListItem>
+      {items.map((data) => (
+        <NavLink
+          key={data.to}
+          {...data}
+          active={location.pathname === data.to}
+        />
+      ))}
     </List>
   );
 }
+
+const NavLink = React.memo(
+  ({
+    to,
+    label,
+    icon: Icon,
+    active = false,
+  }: {
+    to: string;
+    label: string;
+    icon: React.ElementType;
+    active?: boolean;
+  }) => (
+    <ListItem key={to}>
+      <ChakraLink as={ReactRouterLink} to={to}>
+        <Tooltip
+          label={label}
+          aria-label="A tooltip"
+          hasArrow
+          size="md"
+          placement="right-end"
+        >
+          <CustomCard
+            _hover={{ textDecor: "none", color: "green_light" }}
+            _focus={{ outline: "none" }}
+            color={active ? "green_light" : "gray.600"}
+          >
+            <Icon size={24} />
+          </CustomCard>
+        </Tooltip>
+      </ChakraLink>
+    </ListItem>
+  )
+);
 
 export default Sidebar;
